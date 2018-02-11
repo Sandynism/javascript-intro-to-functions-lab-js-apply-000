833bca8b47740709dbbbade8ef18f9343bff5ee5
Sandy Yeung <sandynism@gmail.com>
2018-02-11 19:52:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function shout(string) {
+  string.toUpperCase()
 }
 
 function whisper(string) {
+  string.toLowerCase()
 }
 
 function logShout(string) {
+  console.log string.toUpperCase()
 }
 
 function logWhisper(string) {
